151b41f2ebf7960ca748198690e81d33fc00519f
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2024-06-11 15:35:58 +0300

    add login form

--- src/app/components/main-block/header/header.component.ts
+++ src/app/components/main-block/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginFormComponent } from '../../admin-panel/login-form/login-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'rca-header',
   standalone: true,
   imports: [
+    CommonModule,
     RouterModule,
+    LoginFormComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public isLogin: boolean = false;
 
+  public loginVisible(){
+    this.isLogin = !this.isLogin;
+  }
 }
